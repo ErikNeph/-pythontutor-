18517e271bcb0c3acc7180a3c19da46ffff5fca0
Erik <128590288+ErikNeph@users.noreply.github.com>
2023-04-17 01:19:51 +0600

    Add files via upload

---
+++ 3.Вычисления. Задача 12.Гипотенуза.py
@@ -0,0 +1,10 @@
+# Условие
+# Дано два числа a и b. Выведите гипотенузу треугольника с заданными катетами.
+
+print((int(input()) ** 2 + int(input()) ** 2) ** 0.5)
+
+'''import math
+a = int(input())
+b = int(input())
+c = math.sqrt(a * a + b * b)
+print(c)'''
