5ae868b2325340204bc29a11ffedcbc77f923b7e
Erik <128590288+ErikNeph@users.noreply.github.com>
2023-04-23 19:26:10 +0600

    Delete 1.Ввод и вывод. Задача 5.Следующее и предыдущее .py

--- 1.Ввод и вывод. Задача 5.Следующее и предыдущее .py
+++
@@ -1,9 +0,0 @@
-# Условие
-# Напишите программу, которая считывает целое число и выводит текст, аналогичный приведенному в примере
-# (пробелы важны!).
-# The next number for the number 1534 is 1535.
-# The previous number for the number 1534 is 1533.
-
-Number = int(input())
-print('The next number for the number {0} is {1}.'.format(Number, Number + 1))
-print('The previous number for the number {0} is {1}.'.format(Number, Number - 1))
