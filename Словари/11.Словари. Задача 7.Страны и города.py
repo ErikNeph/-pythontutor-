d3c7821d1079e00d607c1db1c8665ca46ff5e81e
Erik <128590288+ErikNeph@users.noreply.github.com>
2023-06-19 11:48:37 +0600

    Add files via upload

---
+++ Словари/11.Словари. Задача 7.Страны и города.py
@@ -0,0 +1,13 @@
+# Условие
+# Дан список стран и городов каждой страны. 
+# Затем даны названия городов. Для каждого города укажите, в какой стране он находится.
+
+
+motherland = {}
+for i in range(int(input())):
+	country, *cities = input().split()
+	for city in cities:
+		motherland[city] = country
+		
+for i in range(int(input())):
+	print(motherland[input()])
